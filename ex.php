14dc7fa34f761ca26fdf73770bd30064f8109045
hacastle <99904786+hacastle@users.noreply.github.com>
2022-05-25 04:58:10 +0900

    Add files via upload

---
+++ ex.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <div> WEB_TEST</div>
+	<form method="get" action="ex2.php">
+		id : <input type="text" name="id">
+		pw : <input type="text" name="pw">
+		<input type="submit">
+	</form>
+</body>
+</html>
---
+++ ex2.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <div> WEB_TEST</div>
+    <p>ID: <?php echo $_GET["id"]; ?></p>
+    <p>Age: <?php echo $_GET["age"]; ?></p>
+</body>
+</html>
